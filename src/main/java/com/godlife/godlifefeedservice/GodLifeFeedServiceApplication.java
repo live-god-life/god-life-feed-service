e91fcf6bcec7f902015cb06b8a45378843be9da7
fDevJc <fdevjc@gmail.com>
2022-10-13 20:51:28 +0900

    init commit

---
+++ src/main/java/com/godlife/godlifefeedservice/GodLifeFeedServiceApplication.java
@@ -0,0 +1,13 @@
+package com.godlife.godlifefeedservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GodLifeFeedServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GodLifeFeedServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/godlife/godlifefeedservice/GodLifeFeedServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.godlife.godlifefeedservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GodLifeFeedServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
