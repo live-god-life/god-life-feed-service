d413c93c64220847704fa44623ef51da0d3405c9
fDevJc <fdevjc@gmail.com>
2022-10-31 21:02:03 +0900

    feat: DTO 개발

---
+++ src/main/java/com/godlife/feedservice/dto/UserDto.java
@@ -0,0 +1,12 @@
+package com.godlife.feedservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class UserDto {
+    private Long id;
+    private String nickname;
+    private String image;
+}
