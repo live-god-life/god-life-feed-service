e0c0cdd712fa74559fbba3f3ba8ef8c3f6a2c568
fDevJc <fdevjc@gmail.com>
2022-10-31 20:56:47 +0900

    feat: Feed 레파지토리 개발

--- src/main/java/com/godlife/feedservice/repository/FeedRepository.java
+++ src/main/java/com/godlife/feedservice/repository/FeedRepository.java
@@ -1,4 +1,16 @@
 package com.godlife.feedservice.repository;
 
-public interface FeedRepository {
+import com.godlife.feedservice.domain.Feed;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface FeedRepository extends JpaRepository<Feed, Long> {
+    List<Feed> findAllByCategory(Pageable page, String category);
+
+    @Query(nativeQuery = true, value = "SELECT * FROM feed as f WHERE f.feed_id in :ids")
+    List<Feed> findByIds(Pageable page, @Param("ids") List<Long> ids);
 }
