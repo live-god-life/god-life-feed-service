eb428753190fe8accdb19b092ccdb932aa9da337
fDevJc <fdevjc@gmail.com>
2022-10-31 20:59:03 +0900

    feat: 마인드셋, 투두 엔티티 개발

--- src/main/java/com/godlife/feedservice/domain/TodoFolder.java
+++ src/main/java/com/godlife/feedservice/domain/TodoFolder.java
@@ -1,4 +1,33 @@
 package com.godlife.feedservice.domain;
 
-public class TodoFolder {
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.CascadeType;
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@DiscriminatorValue("FOLDER")
+@Entity
+public class TodoFolder extends Todo{
+    @OneToMany(mappedBy = "todo", cascade = CascadeType.ALL, orphanRemoval = true)
+    private final List<TodoTask> todos = new ArrayList<>();
+
+    protected TodoFolder(String title, Integer depth, Integer orderNumber, Feed feed) {
+        super(title, depth, orderNumber, feed);
+    }
+
+    public static TodoFolder createTodoFolder(String title, int depth, int orderNumber, Feed feed) {
+        return new TodoFolder(title, depth, orderNumber, feed);
+    }
+
+    public void addChildTodos(List<TodoTask> todos) {
+        this.todos.addAll(todos);
+    }
 }
